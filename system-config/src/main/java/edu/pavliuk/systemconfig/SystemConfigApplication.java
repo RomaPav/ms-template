178790e86cbc0f6095540ab5f4dbd972a2b0eba3
Роман Павлюк <romatv321@gmail.com>
2023-11-04 21:39:00 +0200

    Initial commit

---
+++ system-config/src/main/java/edu/pavliuk/systemconfig/SystemConfigApplication.java
@@ -0,0 +1,13 @@
+package edu.pavliuk.systemconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemConfigApplication.class, args);
+    }
+
+}
